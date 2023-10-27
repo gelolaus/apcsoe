5bfc402de39318ea72e4f72e61c7abc68c74ca8c
Angelo Laus <angelolausofcl@gmail.com>
2023-10-27 09:47:27 +0800

    feat: create handson

---
+++ PROGLOD_Codes/Hands_On/ttt/main.cpp
@@ -0,0 +1,24 @@
+// Created by Angelo John Benedict Laus
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+    char board[3][3]{{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
+
+
+int main() {
+
+    
+    cout << "" << endl;
+
+    return 0;
+
+}
+
+int board() {
+
+    
+
+}
